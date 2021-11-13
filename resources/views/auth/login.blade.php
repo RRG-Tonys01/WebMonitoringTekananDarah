f1990eada7817aa89b6727ae0b680959484943ea
Tonys01 <sembiringtoni123@gmail.com>
2021-11-13 22:23:33 +0700

    Pembuatan database, homepage, login, controller

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,32 @@
 <link rel="stylesheet" href="/css/login.css">
 
 <div class="center">
+    @if(session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{session('loginError')}}
+            {{-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+            </button> --}}
+        </div>
+    @endif
+
     <h1>Login</h1>
-    <form method="POST" action="{{route('login.custom')}}">
+    <form method="POST" action="/login">
         @csrf
         <div class="inputbox">
-            <input type="text" required="required" name="username">
+            <input type="text" name="username" id="username" autofocus required>
             <span>Username</span>
+            {{-- @error('username')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror --}}
         </div>
         <div class="inputbox">
-            <input type="password" required="required" name="password">
+            <input type="password" name="password" id="password" required>
             <span>Password</span>
         </div>
         <div class="inputbox">
-            <input type="button" value="submit">
+            <input type="submit" value="submit">
         </div>
-
     </form>
 </div>
